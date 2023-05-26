0e758191d5a69d98288ad2c414d8962a602966fc
Leslie Lopez <leslie2515lopez@gmail.com>
2023-05-26 00:10:16 -0600

    Updated exercise detail and fetchdata components

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
-import { exerciseOptions, fetchData } from "../utils/fetchData";
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -22,6 +23,12 @@ const ExerciseDetail = () => {
         exerciseOptions
       );
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData);
     };
 
     fetchExercisesData();
@@ -29,8 +36,11 @@ const ExerciseDetail = () => {
 
   return (
     <Box>
-      <Detail />
-      <ExerciseVideos />
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
       <SimilarExercises />
     </Box>
   );
