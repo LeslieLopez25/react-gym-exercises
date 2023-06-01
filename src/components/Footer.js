e531f90056127e8dd0fbe6eab8eb121a6616a462
Leslie Lopez <leslie2515lopez@gmail.com>
2023-05-31 23:56:29 -0600

    Updated logo and hero banner images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#fff3f4">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <img src={Logo} alt="logo" width="200px" height="200px" />
         <Typography variant="h5" pb="40px" mt="20px">
-          kdljajdldkla
+          Elite Surge
         </Typography>
       </Stack>
     </Box>
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import HeroBannerImage from "../assets/images/banner.png";
+import HeroBannerImage from "../assets/images/banner.jpg";
 
 const HeroBanner = () => (
   <Box
