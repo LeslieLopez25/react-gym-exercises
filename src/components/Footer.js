8b9494e62830f525adf7485222174d2258124e37
Leslie Lopez <leslie2515lopez@gmail.com>
2023-06-08 23:50:20 -0600

    Update footer css component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <Box mt="80px">
+    <Box mt="10px">
       <Stack gap="40px" alignItems="center" px="40px" pt="24px">
         <img src={Logo} alt="logo" width="200px" height="200px" />
-        <Typography variant="h5" pb="40px" mt="20px">
-          Elite Surge
+        <Typography variant="h5" pb="10px" color="#fff600">
+          Elite Surge © {new Date().getFullYear()}
         </Typography>
       </Stack>
     </Box>
